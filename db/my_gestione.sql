129fe736e47a24c543d5a3058b892f419c95f7e5
asdf1899 <araid.anas99@gmail.com>
2018-09-20 03:30:17 +0200

    Fix sql

--- db/my_gestione.sql
+++ db/my_gestione.sql
@@ -2,48 +2,6 @@ DROP DATABASE IF EXISTS my_gestione;
 CREATE DATABASE IF NOT EXISTS my_gestione DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci;
 USE my_gestione;
 
-
-CREATE TABLE t_rapportoVVF (
-  ID 		                    BIGINT				NOT NULL 	AUTO_INCREMENT,
-  ID_Rapporto               BIGINT				NOT NULL,
-  OraUscita                 TIMESTAMP,
-  OraRientro                TIMESTAMP,
-  Data                      DATE,
-  Urgente                   BOOLEAN,
-  OperazioneEseguite        TEXT,
-  Osservazioni              TEXT,
-  FK_Localita               BIGINT,
-  FK_GeneralitaColpito      BIGINT,
-  FK_ProvChiamata           BIGINT,
-  FK_TipoChiamata           BIGINT,
-  FK_SoccorsiEsterni        BIGINT,
-  FK_Responsabile           BIGINT				NOT NULL,
-  FK_Compilatore            BIGINT				NOT NULL,
-  PRIMARY KEY(ID),
-  FOREIGN KEY(FK_Localita)    REFERENCES t_localita(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_GeneralitaColpito)    REFERENCES t_generalitaColpiti(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_ProvChiamata)    REFERENCES t_provChiamata(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_TipoChiamata)    REFERENCES t_tipoChiamata(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_AltriSoccorsi)    REFERENCES t_soccorsiEsterni(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_Responsabile)    REFERENCES t_vigili(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_Compilatore)    REFERENCES t_vigili(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
-
-
 CREATE TABLE t_vigili (
   ID 		           BIGINT				NOT NULL 	AUTO_INCREMENT,
   Nome	 			     VARCHAR(50),
@@ -77,18 +35,7 @@ CREATE TABLE t_mezzi (
   PRIMARY KEY(ID)
 ) ENGINE = InnoDB;
 
-CREATE TABLE t_mezziIntervenuti (
-  ID 		            BIGINT				NOT NULL 	AUTO_INCREMENT,
-  FK_RapportoVVF    BIGINT,
-  FK_Mezzo          BIGINT,
-  PRIMARY KEY(ID),
-  FOREIGN KEY(FK_RapportoVVF)    REFERENCES t_rapportoVVF(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  FOREIGN KEY(FK_Mezzo)    REFERENCES t_mezzi(ID)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+
 
 CREATE TABLE t_generalitaColpiti (
   ID 		           BIGINT				NOT NULL 	AUTO_INCREMENT,
@@ -108,6 +55,56 @@ CREATE TABLE t_soccorsiEsterni (
   PRIMARY KEY(ID)
 ) ENGINE = InnoDB;
 
+
+CREATE TABLE t_rapportoVVF (
+  ID 		                    BIGINT				NOT NULL 	AUTO_INCREMENT,
+  ID_Rapporto               BIGINT				NOT NULL,
+  OraUscita                 TIMESTAMP,
+  OraRientro                TIMESTAMP,
+  Data                      DATE,
+  Urgente                   BOOLEAN,
+  OperazioneEseguite        TEXT,
+  Osservazioni              TEXT,
+  FK_Localita               BIGINT,
+  FK_GeneralitaColpito      BIGINT,
+  FK_ProvChiamata           BIGINT,
+  FK_TipoChiamata           BIGINT,
+  FK_Responsabile           BIGINT				NOT NULL,
+  FK_Compilatore            BIGINT				NOT NULL,
+  PRIMARY KEY(ID),
+  FOREIGN KEY(FK_Localita)    REFERENCES t_localita(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(FK_GeneralitaColpito)    REFERENCES t_generalitaColpiti(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(FK_ProvChiamata)    REFERENCES t_provChiamata(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(FK_TipoChiamata)    REFERENCES t_tipoChiamata(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(FK_Responsabile)    REFERENCES t_vigili(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(FK_Compilatore)    REFERENCES t_vigili(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
+CREATE TABLE t_mezziIntervenuti (
+  ID 		            BIGINT				NOT NULL 	AUTO_INCREMENT,
+  FK_RapportoVVF    BIGINT,
+  FK_Mezzo          BIGINT,
+  PRIMARY KEY(ID),
+  FOREIGN KEY(FK_RapportoVVF)    REFERENCES t_rapportoVVF(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(FK_Mezzo)    REFERENCES t_mezzi(ID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
 CREATE TABLE t_soccorsiIntervenuti (
   ID 		              BIGINT				NOT NULL 	AUTO_INCREMENT,
   FK_RapportoVVF      BIGINT,
@@ -121,75 +118,75 @@ CREATE TABLE t_soccorsiIntervenuti (
     ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
-INSERT INTO t_provChiamata VALUES ('Selettiva Centrale Trento');
-INSERT INTO t_provChiamata VALUES ('Telefono Centrale Trento');
-INSERT INTO t_provChiamata VALUES ('Radio Centrale Trento');
-INSERT INTO t_provChiamata VALUES ('Selettiva Corpo');
-INSERT INTO t_provChiamata VALUES ('Comandante');
-INSERT INTO t_provChiamata VALUES ('Vicecomandante');
-INSERT INTO t_provChiamata VALUES ('Passante');
-INSERT INTO t_provChiamata VALUES ('Telefono');
-INSERT INTO t_provChiamata VALUES ('Servizi');
-INSERT INTO t_provChiamata VALUES ('Altro');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Selettiva Centrale Trento');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Telefono Centrale Trento');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Radio Centrale Trento');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Selettiva Corpo');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Comandante');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Vicecomandante');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Passante');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Telefono');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Servizi');
+INSERT INTO t_provChiamata (Provenienza) VALUES ('Altro');
 
-INSERT INTO t_mezzi VALUES ('VW Interventi Tecnici');
-INSERT INTO t_mezzi VALUES ('VW Pinze');
-INSERT INTO t_mezzi VALUES ('Daily Interventi Tecnici');
-INSERT INTO t_mezzi VALUES ('Autobotte Volvo');
-INSERT INTO t_mezzi VALUES ('Autobotte 180');
-INSERT INTO t_mezzi VALUES ('Minibotte');
-INSERT INTO t_mezzi VALUES ('Autoscala');
-INSERT INTO t_mezzi VALUES ('Snorkel');
-INSERT INTO t_mezzi VALUES ('Autogru MAN');
-INSERT INTO t_mezzi VALUES ('Nissan Terrano');
-INSERT INTO t_mezzi VALUES ('Land Rover TD5');
-INSERT INTO t_mezzi VALUES ('Land Rover TD4');
-INSERT INTO t_mezzi VALUES ('Mitsubishi Pickup');
-INSERT INTO t_mezzi VALUES ('Gommone');
-INSERT INTO t_mezzi VALUES ('Fiat Punto');
-INSERT INTO t_mezzi VALUES ('Daily Trasporto');
-INSERT INTO t_mezzi VALUES ('VW trasp. persone');
-INSERT INTO t_mezzi VALUES ('VW trasp. unione');
-INSERT INTO t_mezzi VALUES ('Carrello trasporto mezzi');
-INSERT INTO t_mezzi VALUES ('Pompa Ziegler');
-INSERT INTO t_mezzi VALUES ('Carrello Ziegler');
-INSERT INTO t_mezzi VALUES ('Carrello incendi boschivi');
-INSERT INTO t_mezzi VALUES ('Pompa Rosenbauer');
-INSERT INTO t_mezzi VALUES ('Carrello gruppo elettrogeno');
-INSERT INTO t_mezzi VALUES ('Idrovora');
+INSERT INTO t_mezzi (Descrizione) VALUES ('VW Interventi Tecnici');
+INSERT INTO t_mezzi (Descrizione) VALUES ('VW Pinze');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Daily Interventi Tecnici');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Autobotte Volvo');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Autobotte 180');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Minibotte');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Autoscala');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Snorkel');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Autogru MAN');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Nissan Terrano');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Land Rover TD5');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Land Rover TD4');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Mitsubishi Pickup');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Gommone');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Fiat Punto');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Daily Trasporto');
+INSERT INTO t_mezzi (Descrizione) VALUES ('VW trasp. persone');
+INSERT INTO t_mezzi (Descrizione) VALUES ('VW trasp. unione');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Carrello trasporto mezzi');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Pompa Ziegler');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Carrello Ziegler');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Carrello incendi boschivi');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Pompa Rosenbauer');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Carrello gruppo elettrogeno');
+INSERT INTO t_mezzi (Descrizione) VALUES ('Idrovora');
 
-INSERT INTO t_soccorsiEsterni VALUES ('CC Pergine');
-INSERT INTO t_soccorsiEsterni VALUES ('CC Borgo');
-INSERT INTO t_soccorsiEsterni VALUES ('Polizia Municipale');
-INSERT INTO t_soccorsiEsterni VALUES ('Polizia Stradale');
-INSERT INTO t_soccorsiEsterni VALUES ('Medico');
-INSERT INTO t_soccorsiEsterni VALUES ('Ambulanza');
-INSERT INTO t_soccorsiEsterni VALUES ('Gestione Strade');
-INSERT INTO t_soccorsiEsterni VALUES ('Servizi forestali');
-INSERT INTO t_soccorsiEsterni VALUES ('AMNU-Serv. fun.');
-INSERT INTO t_soccorsiEsterni VALUES ('CSA');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('CC Pergine');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('CC Borgo');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('Polizia Municipale');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('Polizia Stradale');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('Medico');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('Ambulanza');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('Gestione Strade');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('Servizi forestali');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('AMNU-Serv. fun.');
+INSERT INTO t_soccorsiEsterni (Descrizione) VALUES ('CSA');
 
-INSERT INTO t_tipoChiamata VALUES ('Allagamento');
-INSERT INTO t_tipoChiamata VALUES ('Apertura porta');
-INSERT INTO t_tipoChiamata VALUES ('Bonifica insetti');
-INSERT INTO t_tipoChiamata VALUES ('Fuga di gas');
-INSERT INTO t_tipoChiamata VALUES ('Incendio abitazione');
-INSERT INTO t_tipoChiamata VALUES ('Incendio autoveicolo');
-INSERT INTO t_tipoChiamata VALUES ('Incendio sterpaglie');
-INSERT INTO t_tipoChiamata VALUES ('Incendio cassonetto');
-INSERT INTO t_tipoChiamata VALUES ('Altri incendi');
-INSERT INTO t_tipoChiamata VALUES ('Incidente stradale');
-INSERT INTO t_tipoChiamata VALUES ('Inquinamento');
-INSERT INTO t_tipoChiamata VALUES ('Manifestazioni');
-INSERT INTO t_tipoChiamata VALUES ('Prevenzione');
-INSERT INTO t_tipoChiamata VALUES ('Pulizia sede stradale');
-INSERT INTO t_tipoChiamata VALUES ('Recupero carico');
-INSERT INTO t_tipoChiamata VALUES ('Sblocco ascensore');
-INSERT INTO t_tipoChiamata VALUES ('Servizio reperibilità');
-INSERT INTO t_tipoChiamata VALUES ('Soccorso animali');
-INSERT INTO t_tipoChiamata VALUES ('Soccorso persona');
-INSERT INTO t_tipoChiamata VALUES ('Soccorso tecnico generico');
-INSERT INTO t_tipoChiamata VALUES ('Sopralluogo incendio');
-INSERT INTO t_tipoChiamata VALUES ('Supporto elicottero');
-INSERT INTO t_tipoChiamata VALUES ('Taglio Pianta');
-INSERT INTO t_tipoChiamata VALUES ('Altro');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Allagamento');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Apertura porta');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Bonifica insetti');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Fuga di gas');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Incendio abitazione');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Incendio autoveicolo');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Incendio sterpaglie');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Incendio cassonetto');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Altri incendi');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Incidente stradale');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Inquinamento');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Manifestazioni');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Prevenzione');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Pulizia sede stradale');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Recupero carico');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Sblocco ascensore');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Servizio reperibilità');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Soccorso animali');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Soccorso persona');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Soccorso tecnico generico');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Sopralluogo incendio');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Supporto elicottero');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Taglio Pianta');
+INSERT INTO t_tipoChiamata (Tipologia) VALUES ('Altro');
